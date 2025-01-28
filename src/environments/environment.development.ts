7183c7b454e4b428f055bfcceadce7d484c338dd
satyam-pageup <schouksey@pageupsoft.com>
2025-01-28 18:31:47 +0530

    Phase-2 : Refactor dashboard component HTML and TypeScript; restore datePicker method functionality and update order selection options

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
 
-    firebase : {
+    firebase: {
         apiKey: "AIzaSyCAH3JSPltdvmP-AaPG7pJaqlsFe1rCF9A",
         authDomain: "mirai-logistics.firebaseapp.com",
         projectId: "mirai-logistics",
@@ -9,11 +9,17 @@ export const environment = {
         appId: "1:165717162890:web:f73655ac23dfe90369a673",
         measurementId: "G-6WPYJKBEGL"
     },
-    vapidKey:"BGQTV_dtJpCHMOsXIyqHDM9kPUDEG-CkViSu55_BOAcdk2-ewp9EEdXRi4148uNjoNrJFn_X-Fo-uXjQeHGjFM8",
+    vapidKey: "BGQTV_dtJpCHMOsXIyqHDM9kPUDEG-CkViSu55_BOAcdk2-ewp9EEdXRi4148uNjoNrJFn_X-Fo-uXjQeHGjFM8",
 
     // baseUrl: "http://pageupsoft-001-site13.ktempurl.com/api/",
-    // baseUrl: "https://miraidev-cqe0fpeqcycdbqdb.centralindia-01.azurewebsites.net/api/",
-    baseUrl: "http://192.168.29.28:5443/api/",
+
+    //development
+    baseUrl: "https://miraidev-cqe0fpeqcycdbqdb.centralindia-01.azurewebsites.net/api/",
+
+    //production
+    // baseUrl: "https://miraiprod-d5eec2a4czb7fqcf.centralindia-01.azurewebsites.net/api/",
+
+    // baseUrl: "http://192.168.29.28:5443/api/",
     // http://192.168.1.41:5443/swagger/index.html
     // http://192.168.1.41:5443/swagger/index.html
 
